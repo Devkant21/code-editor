1e708801ab7c56ecca39f2279c6912f3a51cb0b1
Devkant <devkant21@gmail.com>
2022-06-04 20:14:11 +0530

    updates

--- code.js
+++ code.js
@@ -3,13 +3,23 @@ const htmlEditor = CodeMirror(document.querySelector(".editor .code .html-code")
     tabSize:4,
     mode:"xml"
 });
-const cssEditor = CodeMirror(document.querySelector(".editor .code .html-code"), {
+const cssEditor = CodeMirror(document.querySelector(".editor .code .css-code"), {
     lineNumbers:true,
     tabSize:4,
     mode:"css"
 });
-const jsEditor = CodeMirror(document.querySelector(".editor .code .html-code"), {
+const jsEditor = CodeMirror(document.querySelector(".editor .code .js-code"), {
     lineNumbers:true,
     tabSize:4,
     mode:"javascript"
+});
+document.querySelector("#run-btn").addEventListener("click",function(){
+    let htmlCode = htmlEditor.getValue();
+    let cssCode = "<style>" + cssEditor.getValue() + "</style>";
+    let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
+    let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+    previewWindow.open()
+    previewWindow.write(htmlCode+cssCode+jsCode);
+    previewWindow.close();
+
 });
